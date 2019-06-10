c0a6cc2143ffd563c2ed83190d362dd130a6a9ae
Brett <brett.castellanos@gmail.com>
2019-06-10 13:42:35 -0500

    Add sys.path appending and BA_URLS global dict

--- src/ratings_scraper.py
+++ src/ratings_scraper.py
@@ -1,10 +1,29 @@
+import os
+import sys
+if os.path.dirname(__file__) not in sys.path:
+    print("Appendeded current path to sys.path")
+    sys.path.append(os.path.dirname(__file__))
+
 import argparse
 import boto3
 import json
 import decimal
-
 # import pandas as pd
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
+
+
+
+# The basic URLs that will be scraped.
+# Every URL except for top_rated the item enclosed in <>
+# must be replaced with an appropriate value.
+
+BA_URLS = {
+    'top_rated': 'https://www.beeradvocate.com/lists/top/'
+    'style': 'https://www.beeradvocate.com/lists/style/<style_id>'
+    'location:': 'https://www.beeradvocate.com/lists/<loc>'
+    'brewery': 'https://www.beeradvocate.com/beer/profile/<brewery_id>'
+    'beer': 'https://www.beeradvocate.com/beer/profile/<brewery_id>/<beer_id>'
+}
 
 
 def main():
@@ -13,18 +32,20 @@ def main():
     )
     arg_parser.parse_args()
 
-    dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
-    table = dynamodb.Table('beer_ratings')
-    print(type(table))
-    beer = {
-        'beer': 'Sensory Overload',
-        'brewery': 'Olögy',
-        'rating': decimal.Decimal('4.9')
-    }
+    # dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+    # table = dynamodb.Table('beer_ratings')
+    # print(type(table))
+    # beer = {
+    #     'beer': 'Sensory Overload',
+    #     'brewery': 'Olögy',
+    #     'rating': decimal.Decimal('4.9')
+    # }
 
-    put_beer(beer, table, True)
+    # put_beer(beer, table, True)
 
     return 0
+
+def get_style_ids():
 
 
 def put_beer(beer, table, print_message=False):
@@ -49,6 +70,9 @@ def put_beer(beer, table, print_message=False):
         print(json.dumps(response, indent=4, cls=DecimalEncoder))
 
 
+# The following class is taken from the AWS Tutorial found at:
+# https://docs.aws.amazon.com/amazondynamodb/
+# latest/developerguide/GettingStarted.Python.03.html
 # Helper class to convert a DynamoDB item to JSON.
 class DecimalEncoder(json.JSONEncoder):
     def default(self, o):
@@ -58,11 +82,6 @@ class DecimalEncoder(json.JSONEncoder):
             else:
                 return int(o)
         return super(DecimalEncoder, self).default(o)
-
-
-def hello_world():
-    print('Hello World!')
-    return None
 
 
 if __name__ == "__main__":
