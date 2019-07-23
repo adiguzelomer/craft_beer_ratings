2b2d2ba84b0208fcd92d9f42be03e271151dccaf
Brett <brett.castellanos@gmail.com>
2019-07-23 14:23:36 -0500

    write create_collab_rec.py

--- src/create_collab_rec.py
+++ src/create_collab_rec.py
@@ -1,4 +1,4 @@
-from src.collaborative.collaborative import CollaborativeRecommender
+from collaborative.collaborative import CollaborativeRecommender
 import argparse
 import pickle
 import boto3
@@ -7,36 +7,34 @@ import logging
 import numpy as np
 import pandas as pd
 
+
 def main():
     """Creates and pickles a CollaborativeRecommender.
     """
     arg_parser = argparse.ArgumentParser(
         description='Creates and pickles a CollaborativeRecommender.'
     )
+
     arg_parser.add_argument(
-        '--n_beers', default=None, type=int, help='The number of beers to sample.'
-    )
-    arg_parser.add_argument(
-        '--beers_csv', default=os.environ['BEERS_CSV'],
-        help='The location of the beers csv.'
-    )
-    arg_parser.add_argument(
-        '--reviews_csv', default=os.environ['REVIEWS_CSV'],
+        '--reviews_csv', default='data/1-cleaned/cleaned_reviews.csv',
         help='The location of the reviews csv.'
     )
-    arg_parser.add_argument(
-        '--max_df', default=.95, type=float,
-        help='The maximum document frequency.'
-    )
-    arg_parser.add_argument(
-        '--max_features', default=5000, type=int,
-        help='The maximum number of features to consider.'
-    )
-    arg_parser.add_argument(
-        '--n_topics', default=15, type=int,
-        help='The maximum number of topics to assess.'
-    )
     args = arg_parser.parse_args()
+    print('Loading the data')
+    reviews = pd.read_csv('data/1-clean/clean_reviews.csv',
+                          usecols=['brew_beer', 'beer', 'overall', 'author'])
+
+    print('Fitting the Model')
+    rec = CollaborativeRecommender(50)
+    rec.fit(reviews)
+
+    print('Pickling the model')
+    with open('models/2-collaborative-item-item/collab_rec.pkl', 'wb') as f:
+        pickle.dump(rec, f)
+
+    print('Uploading to S3')
+    upload_file('models/2-collaborative-item-item/collab_rec.pkl', 'brett-craft-beer')
+
     return 0
 
 # Copied from boto3 tutorials
