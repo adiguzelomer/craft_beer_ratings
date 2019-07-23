2be03bf0b0a1925c7072521cb9255363c532159d
Brett <brett.castellanos@gmail.com>
2019-07-23 13:03:46 -0500

    Add functionality to CollaborativeRecommender

--- src/collaborative/collaborative.py
+++ src/collaborative/collaborative.py
@@ -1,10 +1,15 @@
-
+import pandas as pd
+from scipy.sparse import csr_matrix
+from sklearn.metrics.pairwise import cosine_similarity
 
 class CollaborativeRecommender:
     """A user-user collaborative recommender.
     """
-    def __init__(self):
-        self.utility_mat, self.users, self.beers = None, None, None
+    def __init__(self, neighborhood_size):
+        self.utility_mat, self.users, self.brew_beers = None, None, None
+        self.neighborhood_size = neighborhood_size
+        self.neighborhoods = None
+        self.similarity_mat = None
 
     def fit(self, reviews: pd.DataFrame) -> None:
         """Fits to the reviews provided.
@@ -18,7 +23,16 @@ class CollaborativeRecommender:
         -------
         None
         """
-        pass
+        pivot_table = reviews.pivot_table(
+            index='brew_beer', columns='author', values='overall')
+        self.utility_mat = csr_matrix(pivot_table.values)
+        self.users = list(pivot_table.columns)
+        self.brew_beers = list(pivot_table.index)
+        self.similarity_mat = cosine_similarity(self.utility_mat)
+
+    def _set_neighborhoods(self):
+        least_to_most_sim_indexes = np.argsort(self.item_sim_mat, 1)
+        self.neighborhoods = least_to_most_sim_indexes[:, -self.neighborhood_size:]
 
     def predict(self, user_vector: np.array) -> list:
         """Given a vector representing a user, returns a list of suggested
